e1097cd1228212aa4d00e8cd8791b28d3772ae27
Samu <samuelitocarquin31@gmail.com>
2025-03-14 20:13:15 -0500

    Se agrega la clase persona

---
+++ src/autonoma/biblioteca/models/Persona.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.biblioteca.models;
+
+/**
+ *
+ * @author MAYERLLI
+ * @since 14/03/2025
+ *  versión 1.0
+ */
+public abstract class Persona {
+    private String nombre;
+    private String documentoIdentidad;
+    private String correoElectronico;
+
+    public Persona(String nombre, String documentoIdentidad, String correoElectronico) {
+        this.nombre = nombre;
+        this.documentoIdentidad = documentoIdentidad;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+    
+    
+}
