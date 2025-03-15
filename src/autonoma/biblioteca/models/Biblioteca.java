7605a985744bf64690c11bff5224335c4f2cb321
Juan Esteban Vera Velez <juanestebanv0612@gmail.com>
2025-03-14 21:20:45 -0500

    Version inicial biblioteca

---
+++ src/autonoma/biblioteca/models/Biblioteca.java
@@ -0,0 +1,41 @@
+package autonoma.biblioteca.models;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Juan Esteban Vera Velez
+ * @since 14/03/2025
+ * versión 1.0
+ */
+public class Biblioteca {
+    
+ 
+    public String mostrarLibros() {
+        return null;
+    }
+    
+    
+    public boolean agregarLibro(Libro libro) {
+        return false;
+    }
+    
+    
+    public Libro buscarLibro(long id) {
+        return null;
+    }
+    
+    
+    public boolean actualizarLibro(long id, Libro libro) {
+        return false;
+    }
+    
+    
+    public boolean eliminarLibro(long id) {
+        return false;
+    }
+    
+   
+    public ArrayList<Libro> obtenerLibrosAlfabeticamente() {
+        return null;
+    }
+}
